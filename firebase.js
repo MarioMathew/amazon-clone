6bde764753e0703d61acbc8d95403bee85f2cdc8
Mario Mathew <mariojosephmathew@gmail.com>
2023-06-06 20:35:11 +0530

    git commit -m" Day1"

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC5E6uVDwCbEQ3F8AGdSXHIGiafBHvN_m0",
+    authDomain: "challenge-efa0b.firebaseapp.com",
+    projectId: "challenge-efa0b",
+    storageBucket: "challenge-efa0b.appspot.com",
+    messagingSenderId: "545142800192",
+    appId: "1:545142800192:web:f5a3e33fbd3674004f04a5",
+    measurementId: "G-44M7JRW4GP"
+  };
---
+++ src/Header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className='header'>
+      <img 
+            className='header_logo'
+            src = "http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+        <div
+        className='header_search'>
+            <input className='header_searchInput' type = "text"/>
+
+            {/*logo*/}
+        </div>
+        
+        <div className='header_nav'>
+          <div className='header_option'>
+            <span className='header__optionLineone'>Hello Guest</span>
+            <span className='header__optionLineTwo'>Sign In</span>
+
+          </div>
+          <div className='header_option'>
+            <span className='header__optionLineone'>Returns</span>
+            <span className='header__optionLineTwo'>& Orders</span>
+          </div>
+
+          <div className='header_option'>
+            <span className='header__optionLineone'>Your</span>
+            <span className='header__optionLineTwo'>Prime</span>
+          </div>
+          
+        </div>
+    </div>
+  )
+}
+
+export default Header
