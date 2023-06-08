6fa1165fbd941b5166823561dda5d158ecfe610a
Mario Mathew <mariojosephmathew@gmail.com>
2023-06-08 15:23:11 +0530

    udpated

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import React from 'react';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-      <div className = "app"> 
-        < Header/>
-        < Home/>
+    <Router>
+      <div className="app"> 
+        <Header />
+        <Routes>
+          <Route path="/checkout" element={<><h1>I am the checkout</h1></>} />
+          <Route path="/" element={<Home />} />
+        </Routes>
         {/* Home */}
-      </div>);
-  }
+      </div>
+    </Router>
+  );
+}
 
 export default App;
